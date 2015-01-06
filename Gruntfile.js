c8364a877e3f46d44b2973f8c37443b134c4499e
galicians <galicians@gmail.com>
2015-01-06 15:04:07 +0000

    rethink added

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,5 @@
+var db = require("secondthought");
+var assert = require("assert");
 
 module.exports = function(grunt) {
 
@@ -15,6 +17,19 @@ module.exports = function(grunt) {
       tasks: ['jshint']
     }
   });
+
+  grunt.registerTask("installDb", function() {
+    var done = this.async();
+    db.connect({db : "membership"}, function(err,db){
+      db.install(['users','logs','sessions'], function(err,tableResult){
+        assert.ok(err === null, err);
+        console.log("DB installed: " + tableResult);
+        done();
+      });
+    });
+
+  });
+
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
